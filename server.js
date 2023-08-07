55110a83a2086ef99c1fa0d09f556bca67b56742
RoshanYeah <roshankallakkavumkel@gmail.com>
2023-08-07 19:19:14 +1000

    We have done it

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ const server = require("http").Server(app);
 app.use(express.json())
 
 server.listen(process.env.PORT || 3030);
+
+var nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: 'roshankallakkavumkel@gmail.com',
+        pass: 'ughpkwkwsincbved',
+    },
+    secure: true,
+})
